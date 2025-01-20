9b397e6f74c087457cabecc2aff27188fb2aba2e
Emerson Millard <emillard.code@gmail.com>
2025-01-19 19:59:09 -0800

    Added comments to challenge attempt.

--- src/main/java/com/project/attempt/LeetCodeAttempt.java
+++ src/main/java/com/project/attempt/LeetCodeAttempt.java
@@ -12,16 +12,23 @@ public class LeetCodeAttempt {
 
     }
 
+    // This method will return the number of 3-character palindromes that can be derived from string s.
     public static int uniqueLength3PalindromicSubsequences(String s) {
 
+        // We will use a HashSet to store the palindromes as the
+        // challenge instructions specified unique strings only.
         HashSet<String> uniquePalindromes = new HashSet<String>();
 
+        // As we are looking for 3-character palindromes (without rearranging their relative order), we will
+        // use three for-loops to increment through the string and try all possible 3-character combinations.
         for (int i = 0; i < s.length(); i++) {
 
             for (int j = i + 1; j < s.length(); j++) {
 
                 for (int k = j + 1; k < s.length(); k++) {
 
+                    // We only need to check the first and last letter for a palindrome. The
+                    // middle letter is irrelevant. If it's a palindrome, add it to the HashSet.
                     if (s.charAt(i) == s.charAt(k)) {
                         uniquePalindromes.add(String.valueOf(s.charAt(i) + s.charAt(j) + s.charAt(k)));
                     }
@@ -32,6 +39,7 @@ public class LeetCodeAttempt {
 
         }
 
+        // Return the size of our HashSet once all 3-character combinations have been tried.
         return uniquePalindromes.size();
 
     }
